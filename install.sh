fa335206436d590b2d5fcd967c8c4f147c13351b
Niclas Thall <niclas.thall@gmail.com>
2015-04-25 20:18:52 +0200

    install.sh isnt quite anymore

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 FILES=$(find . -maxdepth 1 \( ! -iname ".*" ! -iname "install.sh" \))
 
 for f in $FILES; do
-  ln -f -s $PWD${f#.} $HOME/`echo $f | sed 's/\.\//\./g'`
+  cmd="ln -f -s $PWD${f#.} $HOME/`echo $f | sed 's/\.\//\./g'`"
+  echo $cmd
+  eval $cmd
 done
 
