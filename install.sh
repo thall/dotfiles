915672f44f795a3ef9e252eb84fb30883f1492ff
Niclas Thall <niclast.thall@purplescout.se>
2016-04-21 20:38:01 +0200

    Fixed install script to supported multi-level depth of folders

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-FILES=$(find . -maxdepth 1 \( ! -iname ".*" ! -iname "install.sh" \))
+FILES=$(find . -not -path '*/\.*' ! -iname ".*" ! -iname "install.sh")
 
 for f in $FILES; do
-  cmd="ln -f -s $PWD${f#.} $HOME/`echo $f | sed 's/\.\//\./g'`"
-  echo $cmd
-  eval $cmd
+  if [ -d $f ]; then
+    cmd="mkdir -p $HOME/`echo $f | sed 's/\.\//\./g'`"
+    echo $cmd
+    eval $cmd
+  else
+    cmd="ln -f -s ${PWD}${f#.} $HOME/`echo $f | sed 's/\.\//\./g'`"
+    echo $cmd
+    eval $cmd
+  fi
 done
 
